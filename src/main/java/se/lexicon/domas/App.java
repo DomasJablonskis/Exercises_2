e6cbdd785981f95392c5e870f24196ae8ef29c65
Domas <domas.jablonskis@gmail.com>
2020-10-29 16:24:43 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/domas/App.java
@@ -0,0 +1,7 @@
+package se.lexicon.domas;
+
+public class App {
+        public static void main(String[] args) {
+                String myName = "Domas";
+                System.out.println(myName); }
+}
